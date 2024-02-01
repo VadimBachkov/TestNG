c555192f21f4c361e3cad5626a6625a88e5b5a73
VadimBachkov <bachkovvadim@gmail.com>
2024-02-01 19:12:01 +0300

    AutoTest works on selenium driver

---
+++ src/main/java/org/example/test/TheInternetsTest.java
@@ -0,0 +1,20 @@
+package org.example.test;
+
+import org.example.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TheInternetsTest extends  BaseTest{
+
+    HomePage homePage = new HomePage(initDriver());
+    private static final String EXPECTED_URL = ("https://the-internet.herokuapp.com/abtest");
+
+    @Test(priority = 0)
+    public void locatorTest(){
+        HomePage homePage = new HomePage(driver);
+        homePage.clickLink(homePage.abTesting);
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertTrue(actualURL.equals(EXPECTED_URL));
+    }
+
+}
